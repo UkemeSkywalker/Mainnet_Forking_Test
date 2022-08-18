679ba450f7ea23f36cfaf59a460b68de6672001f
ukemeSkywalker <skywalkershadow9@gmail.com>
2022-08-18 22:51:20 +0100

    minor update

--- scripts/interactBlog.ts
+++ scripts/interactBlog.ts
@@ -27,7 +27,7 @@ async function main() {
     // Contract deployed to: 0xb4eA7D9bc09f5B7F5702245467493301C1ebcE0b
     // Txn Hash 1: 0x11f8b0ba198c786043338b4da7fbb36fb772f7f8a50a9b9e1bbc17586f6fc21d
     // Tnx Hash 2: 0xccbb4f10395cdf5807e97062159100274b9f37b93e900d89a9446124bbf29804
-    // Tnx Hash 3: 
+    // Tnx Hash 3: 0x99bb080fbb4fd8e14715899b6fa1dcf41d2bb6416b40b1dde1734f6bfd7cc559
 
 
 }
